c8da58b14a5124656dc92680eb91da8c7af62c61
Cdelim <cdelim@gmail.com>
2020-11-27 05:17:18 +0300

    Rope almost finish

---
+++ Assets/Scripts/InputContoller.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class InputContoller : MonoBehaviour
+{
+    RopeScript ropeScript;
+    private int buttonLayer;
+    private int targetLayer;
+    private bool isButtonUp=false;
+    private Transform button;
+    private float smooth=0.2f;
+    void Start()
+    {
+        buttonLayer=LayerMask.GetMask("End");
+        targetLayer=LayerMask.GetMask("Target");
+        /*GameObject pipe=GameObject.Find("Pipe");
+        ropeScript=pipe.GetComponent<>*/
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        userInput();
+    }
+
+    void userInput(){
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);//Input.GetTouch (0).position
+        RaycastHit hitInfo;
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (Physics.Raycast(ray, out hitInfo,buttonLayer))
+            {
+                if (!isButtonUp)
+                {
+                    print("1");
+                    print(hitInfo.transform.name);
+                    button = hitInfo.transform;
+                    button.position = Vector3.MoveTowards(button.position, button.position + Vector3.up, smooth);
+                    //hitInfo.transform.Translate(Vector3.up * Time.deltaTime);
+                    isButtonUp = true;
+                }
+                else
+                {
+                    print("2");
+                    button.position = Vector3.MoveTowards(button.position, button.position + Vector3.down, smooth);
+                    //hitInfo.transform.Translate(Vector3.down * Time.deltaTime);
+                    isButtonUp = false;
+                }
+            }
+            if (Physics.Raycast(ray, out hitInfo, targetLayer))
+            {
+                if (isButtonUp && button != null)
+                {
+                    print("3");
+                    button.position = Vector3.MoveTowards(button.position, hitInfo.transform.position + new Vector3(0, 1, 0), smooth);
+                    button.position = Vector3.MoveTowards(button.position, hitInfo.transform.position, smooth);
+                }
+            }
+        }
+
+    }
+}
---
+++ Assets/Scripts/SuccesController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent (typeof (RopeScript))]
+public class SuccesController : MonoBehaviour
+{
+    private RopeScript ropeScript;
+    void  Awake()
+    {
+        ropeScript = GetComponent<RopeScript>();
+    }
+
+    // Update is called once per frame
+     void Update()
+    {
+        //base.Update();
+        isSuccesful();
+    }
+
+    void isSuccesful(){
+        Vector3 direction = ropeScript.target.position- transform.position ;
+          Ray ray=new Ray(transform.position,direction);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            Debug.DrawLine(ray.origin, hit.point, Color.red);
+            //ropeScript.DestroyRope();
+        }
+        else {
+            Debug.DrawLine(ray.origin, ray.origin + ray.direction * 100, Color.green);
+        }
+
+    }
+}
