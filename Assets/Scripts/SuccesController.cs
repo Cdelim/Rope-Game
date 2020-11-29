aece0ea822b4b2cbebfc0f49ac0fcc9ac09fc96f
Cdelim <cdelim@gmail.com>
2020-11-29 17:45:52 +0300

    Update SuccesController.cs

--- Assets/Scripts/SuccesController.cs
+++ Assets/Scripts/SuccesController.cs
@@ -10,7 +10,7 @@ public class SuccesController : MonoBehaviour
     void  Awake()
     {
         ropeScript = GetComponent<RopeScript>();
-        ignoreMasks = (LayerMask.GetMask("End","Target","Default",ropeScript.ropeMask.ToString()));
+        ignoreMasks = (LayerMask.GetMask("End","Target","Default"));
         //ignoreMasks += ropeScript.ropeMask;
     }
 
@@ -27,55 +27,38 @@ public class SuccesController : MonoBehaviour
 
     void isSuccesful()
     {
-        //Ray ray = new Ray(ropeScript.joints[i].transform.position-(Vector3.down*5), Vector3.up);
-        //RaycastHit hit;
         //if (Input.GetMouseButtonUp(0)){// Input.GetTouch (0)
-            isKnot = false;
-            for (int i = 1; i < ropeScript.joints.Length; i++) {
-                Ray ray = new Ray(ropeScript.joints[i].transform.position + (Vector3.down * 5), Vector3.up);
-                /*Ray ray = new Ray(ropeScript.joints[i].transform.position , Vector3.down);
-                Ray ray2 = new Ray(ropeScript.joints[i].transform.position, Vector3.up);*/
-                /*Ray ray = new Ray(ropeScript.joints[i].transform.position, new Vector3(0,1,-1));
-                Ray ray2 = new Ray(ropeScript.joints[i].transform.position, new Vector3(0, -1, 1));*/
+        isKnot = false;
+        print("oldu mu");
+        for (int i = 1; i < ropeScript.joints.Length; i++) {
+            Ray ray = new Ray(ropeScript.joints[i].transform.position + (Vector3.down * 5), Vector3.up);
 
-                RaycastHit hit;
-                RaycastHit hit2;
+            RaycastHit hit;
             if (Physics.Raycast(ray, out hit, 20, ~(ignoreMasks))) //|| Physics.Raycast(ray2, out hit2, 3,~ignoreMasks))
             {
-                //print(hit.transform.name);
-                if (hit.transform.name.StartsWith(ropeScript.ropeMask.ToString()))
+                if (!hit.transform.name.StartsWith(LayerMask.LayerToName(ropeScript.joints[i].layer)))
                 {
-                  
-                    return;
+                    isKnot = true;
+                    break;
                 }
-                isKnot = true;
+                print(hit.transform.name);
                 Debug.DrawLine(ray.origin, hit.point , Color.green);
-                //if (Physics.Raycast(ray2, out hit2, 10, ignoreMasks))
-                //Debug.DrawLine(ray2.origin, ray2.origin + ray2.direction * 3, Color.blue);
                 print("123");
             }
             else {
 
                 Debug.DrawLine(ray.origin, ray.origin + ray.direction * 10, Color.blue);
             }
-                /*if (Physics.Raycast(ray2, out hit2, 5, ~ignoreMasks))
-                {
-
-                    print(hit2.transform.name);
-                    isKnot = true;
-                }*/
-                /*else {
-                    Debug.DrawLine(ray2.origin, ray2.origin + ray2.direction * 3, Color.black);
-                }*/
 
 
-            }
-            if (!isKnot) {
-                print("asd");
-                ropeScript.DestroyRope();
-                GameController.numberOfRope--;
-                Destroy(this);
-            }
+        }
+        print(isKnot);
+        if (!isKnot && Time.time>5.0f) {
+            print("asd");
+            ropeScript.DestroyRope();
+            GameController.numberOfRope--;
+            Destroy(this);
+        }
         //}
 
     }
