3279666b76a4876fc33088ca52e20a5134aef708
Cdelim <cdelim@gmail.com>
2020-11-30 01:43:27 +0300

    Buttons can't collision

---
+++ Assets/Scripts/ButtonsScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonsScript : MonoBehaviour
+{
+    [HideInInspector]
+    GameObject[] buttons;
+    void Awake()
+    {
+        buttons = GameObject.FindGameObjectsWithTag("Button");
+    }
+
+
+    public bool isHereFill(Vector3 position) {
+        foreach (GameObject button in buttons) {
+            if (Vector3.Distance(button.transform.position, position) < 0.5f)
+                return true;
+        }
+        return false;
+    }
+}
